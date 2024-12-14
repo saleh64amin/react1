b92d5506c64cb35186ec30353e1927acc1e602e6
saleh64amin <timecube64@gmail.com>
2024-12-14 16:10:54 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './App.css';
+
+
+
+
+const App = () => {
+  return (
+    <div>App
+
+<img src="DALL·E 2024-11-07 18.20.11 - A balanced scale with two pans, one pan filled with healthy food items like fresh vegetables, fruits, nuts, and a bottle of water, while the other pan.webp" alt="ss" />
+
+
+
+    </div>
+  )
+}
+
+export default App
